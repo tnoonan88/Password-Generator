d645d12b5f1f192afafcc7d66768a419b3c934b1
tnoonan <tnoonan88@gmail.com>
2022-01-18 13:27:07 -0500

    JS now has a working password generator with criteria prompts.

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var number = "0123456789"
+var symbol = "!@#$%^&*_-+="
+var charChoices = ""
 
+function generatePassword() {
+  charChoices = ""
+  var lengthChoice = prompt("Character length?")
+  console.log(lengthChoice)
+  if (lengthChoice < 8 || lengthChoice > 128) {
+    alert("Password must be at least 8 and no greater than 128 characters!")
+  return
+  }
+
+  var lowerChoice = confirm("Do you want to include Lower Case?")
+  console.log(lowerChoice)
+  if (lowerChoice === true) {
+    charChoices = charChoices+lowerCase
+    console.log(charChoices)
+  }
+  var upperChoice = confirm("Do you want to include Upper Case?")
+  console.log(upperChoice)
+  if (upperChoice === true) {
+    charChoices = charChoices+upperCase
+    console.log(charChoices)
+  }
+  var numberChoice = confirm("Do you want to include numbers?")
+  console.log(numberChoice)
+  if (numberChoice === true) {
+    charChoices = charChoices+number
+    console.log(charChoices)
+  }
+  var symbolChoice = confirm("Do you want to include symbols?")
+  console.log(symbolChoice)
+  if (symbolChoice === true) {
+    charChoices = charChoices+symbol
+    console.log(charChoices)
+  }
+
+  if (lowerChoice === false && upperChoice === false && numberChoice === false && symbolChoice === false) {
+    alert("You must choose at least one type!")
+    return
+  }
+
+  let password = ""
+  for (let i = 0; i < lengthChoice; i++) {
+    Math.floor(Math.random() * charChoices.length)
+    var randomNum = Math.floor(Math.random() * charChoices.length)
+    charChoices.charAt(randomNum)
+    password+=charChoices.charAt(randomNum)
+    console.log(password)
+  }
+  return password
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
